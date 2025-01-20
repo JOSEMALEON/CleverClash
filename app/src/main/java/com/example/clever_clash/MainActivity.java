7c689f48de2fd067caf04ad6f88c41d734b8aa50
JOSEMALEON <josem.leon.99@gmail.com>
2025-01-20 08:57:38 +0100

    Vuelve a abrirse la app

--- app/src/main/java/com/example/clever_clash/MainActivity.java
+++ app/src/main/java/com/example/clever_clash/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.clever_clash;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.FirebaseApp;
@@ -11,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FirebaseApp.initializeApp(this);
 
         // Redirigir al LoginActivity
         Intent intent = new Intent(this, LoginActivity.class);
